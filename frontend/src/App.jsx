3a9942032657a99afa38a1b6f21bb31023d28f0c
pedrogomes107 <pedromcgomes95@gmail.com>
2024-12-18 21:17:22 +0000

    Frontend - Created Sidebar and RightPanel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import LoginPage from "./pages/auth/login/LoginPage";
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
